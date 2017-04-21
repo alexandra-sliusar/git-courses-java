42c61c96d3d2c994a89292410f3b6054b01e5beb
alexandra-sliusar <alexalioness@gmail.com>
2017-04-21 22:33:59 +0300

    added comments

--- GuessGame/src/main/java/ua/course/java/GuessGame/Controller.java
+++ GuessGame/src/main/java/ua/course/java/GuessGame/Controller.java
@@ -21,12 +21,21 @@ public class Controller {
 
 	}
 
+	/**
+	 * Method that adjust model's fields to begin working with them and print
+	 * information when ready
+	 */
 	public void startGame() {
 		model.setBounds(Constants.MIN_VALUE, Constants.MAX_VALUE);
 		model.setSecretValue();
 		view.printGameIsOn();
 	}
 
+	/**
+	 * Method gets your input and checks whether it is number or not After
+	 * successful guessing prints congratulation message and your previous
+	 * guesses
+	 */
 	public void playGame(Scanner sc) {
 		view.printTryToGuessMessage(model);
 		while (true) {
@@ -43,6 +52,10 @@ public class Controller {
 
 	}
 
+	/**
+	 * Checks whether number is between min and max values if true - changes min
+	 * or max value depending on secret value if false - prints message about it
+	 */
 	public boolean checkGuess(int guess) {
 		model.addGuessInGuessesList(guess);
 		if (guess == model.getSecretValue()) {
@@ -61,12 +74,20 @@ public class Controller {
 		return false;
 	}
 
+	/**
+	 * Method that adjust model's fields to begin working with enumeration and
+	 * print information when ready
+	 */
 	public void startGameWithEnum() {
 		model.setBounds(Constants.MIN_VALUE, Constants.MAX_VALUE);
 		model.setExactSecretValue(69);
 		view.printGameIsOn();
 	}
 
+	/**
+	 * For all values in enum checks them till the enum ends or gets to secret
+	 * value in enumeration
+	 */
 	public void playGameWithEnum() {
 		view.printTryToGuessMessage(model);
 		for (Guesses g : Guesses.values()) {
